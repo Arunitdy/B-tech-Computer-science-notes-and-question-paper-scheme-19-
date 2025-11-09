da00cb737b21848c58f34058934c759ebf6f5e52
Arun <arunmundakkal003@gmail.com>
2025-11-09 14:21:25 +0530

    kl

--- s7/S7 lab/test/test.c
+++ s7/S7 lab/test/test.c
@@ -28,8 +28,50 @@ void accept() {
     }
 }
 
+void con() {
+    int f = -1, r = -1;
+    q[r++] = 1<<start;
+    vis[1<<start] = 1;
+
+    while(f < r) {
+        int s = q[f++];
+
+        for (int a = 0; a < m; a++) {
+            int ns = 0;
+            for (int i = 0; i < n; i++) {
+                if (s & (1<<i)) {
+                    for (int j = 0; j < n; j++) {
+                        if (NFA[i][a][j]) {
+                            ns |= (1<<j);
+                        }
+                    }
+                }
+            }
+
+            DFS[s][a] = ns;
+            if (ns && !vis[ns]) {
+                vis[ns] = 1;
+                q[r++] = ns;
+            }
+        }
+    }
+}
 
 
+void prin(int s) {
+    for (int i = 0; i < n; i++) {
+        if (s & (1<<i)) printf("%dq", i);
+    }
+}
+
+void displai() {
+    for (int s = 0; s < 1<<n; s++) {
+       if (vis[s]){     prin(s);
+            for (int a = 0; a < m; a++) {
+                printf("a"); prin(dfa[s][a]);
+            }}
+    }
+}
 
 
 
